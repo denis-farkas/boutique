4cc84050158e71b70a079ea4fa6c2dcf1627aceb
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2021-02-09 16:46:26 +0100

    ajout inscription

--- views/users/inscription.php
+++ views/users/inscription.php
@@ -14,7 +14,7 @@
         
         <div class="col-md-12 mt-5">
             <div class="row banner pt-3">                               
-                <img class="ml-5 img-fluid mt-5" src="<?php echo WWW_ROOT; ?>public/images/logo.png" alt="Logo">                 
+                <img class="ml-5 img-fluid mt-5" src="<?php echo WWW_ROOT; ?>public/images/logo2.png" alt="Logo">                 
                 <h2 class="mt-5 pt-5 gold">PANAMA HATS<br /><small class="text-muted">Chapeaux de Légende</small></h2>                              
             </div>
         </div>                                       
@@ -35,9 +35,57 @@
                     
                 </div>
             </div>
-            <section class="col-sm-12 col-md-9 my-5 text-center">
+            <section class="col-sm-12 col-md-9 my-5">
                 <div class="container">
-               
+                    <div class="row">
+                        <div class="col-md-6">
+                            <form action="<?php echo WWW_ROOT;?>users/inscription" method="post">
+                            <fieldset>
+                            <legend>INSCRIPTION</legend>
+                            <div class="form-group">
+                                <label for="genre">Civilité</label>
+                                <select class="form-control" id="genre" name="civilite">
+                                    <option>Monsieur</option>
+                                    <option>Madame</option>
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="nom">Nom</label>
+                                <input type="text" class="form-control" id="nom" name="nom" >
+                            </div>
+                            <div class="form-group">
+                                <label for="prenom">Prénom</label>
+                                <input type="text" class="form-control" id="prenom" name="prenom" >
+                            </div>
+
+                            <div class="form-group">
+                                <label for="telephone">Téléphone</label>
+                                <input type="tel" class="form-control" id="telephone" name="telephone" >
+                            </div>
+                            
+                            <div class="form-group">
+                                <label for="email">Adresse email </label>
+                                <input type="email" class="form-control" id="email" name="email">
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" class="form-control" id="password" name="password">
+                            </div>
+                            <div class="form-group">
+                                <label for="confirmPassword">Confirmer Password</label>
+                                <input type="password" class="form-control" id="confirmPassword" name="confirmPassword">
+                            </div>
+                            
+                            <input type="submit" class="btn btn-primary" name="inscrire" value="S'inscrire">
+                            </fieldset>
+                            </form>
+                        </div> 
+                                      
+                        <div class="col-md-6 text-center">
+                            <img class="img-fluid w-75 mt-5" src="<?php echo WWW_ROOT; ?>public/images/tissage2.jpg" alt="tissage">    
+                        </div> 
+                    </div>  
 
 
                
