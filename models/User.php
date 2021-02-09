9bbfd01591ea914343742d52c57a93a22b64ac93
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2021-02-09 11:13:41 +0100

    modif model user

--- models/User.php
+++ models/User.php
@@ -7,12 +7,15 @@ class User {
 
     public function profil($data){
             
-            $this->db->query('UPDATE utilisateurs SET login= :login, password= :password, avatar= :avatar WHERE id= :id');
-            $this->db->bind(':login', $data['login']);
-            $this->db->bind(':password', $data['password']);
-            $this->db->bind(':avatar', $data['avatar'] );
-            $this->db->bind(':id', $data['id']);
-           
+            $this->db->query('UPDATE user SET prenom= :prenom, nom= :nom, civilite= :civilite, telephone= :telephone, email= :email, password= :password WHERE id= :id');
+        $this->db->bind(':prenom', $data['prenom']);
+        $this->db->bind(':nom', $data['nom']);
+        $this->db->bind(':civilite', $data['civilite']);
+        $this->db->bind(':telephone', $data['telephone']);
+        $this->db->bind(':email', $data['email']);
+        $this->db->bind(':password', $data['password']);
+        $this->db->bind(':id_user', $data['id_user']);
+                   
             //Execute function
         if ($this->db->execute()) {
             return true;
@@ -22,27 +25,22 @@ class User {
     }  
     
     public function inscription($data) {
-        $creation= date("Y-m-d");
-        $intranet= $data['login'].'@intranet';
-        $this->db->query('INSERT INTO utilisateurs (login, password, email, intranet, avatar, naissance,
-         creation, genre, role, blocage, periode_blocage) VALUES(:login, :password, :email, :intranet,:avatar, :naissance, :creation, :genre, :role, :blocage, :periode_blocage)');
+        $registre= date("Y-m-d");
+       
+        $this->db->query('INSERT INTO user (prenom, nom, civilite, telephone, email, password, is_admin, date_registre) VALUES(:prenom, :nom, :civilite, :telephone, :email, :password, :is_admin, :date_registre)');
 
 
         //Bind values
-        $this->db->bind(':login', $data['login']);
-        $this->db->bind(':password', $data['password']);
+        $this->db->bind(':prenom', $data['prenom']);
+        $this->db->bind(':nom', $data['nom']);
+        $this->db->bind(':civilite', $data['civilite']);
+        $this->db->bind(':telephone', $data['telephone']);
         $this->db->bind(':email', $data['email']);
-        $this->db->bind(':intranet', $intranet);
-        $this->db->bind(':avatar', $data['avatar']);
-        $this->db->bind(':naissance', $data['naissance']);
-        $this->db->bind(':creation', $creation);
-        $this->db->bind(':genre', $data['genre']);
-        $this->db->bind(':role', $data['role']);
-        $this->db->bind(':blocage', $data['blocage']);
-        $this->db->bind(':periode_blocage', $data['periode_blocage']);
-       
-        
-        //Execute function
+        $this->db->bind(':password', $data['password']);
+        $this->db->bind(':is_admin', $data['is_admin']);
+        $this->db->bind(':date_registre', $registre);
+            
+                //Execute function
         if ($this->db->execute()) {
             return true;
         } else {
@@ -51,11 +49,11 @@ class User {
     }
 
 
-    public function connexion($login, $password) {
-        $this->db->query('SELECT * FROM utilisateurs WHERE login = :login');
+    public function connexion($email, $password) {
+        $this->db->query('SELECT * FROM user WHERE email = :email');
 
         //Bind 
-        $this->db->bind(':login', $login);
+        $this->db->bind(':email', $email);
        
         //méthode row comme objet de database
         $row = $this->db->single();
@@ -71,21 +69,12 @@ class User {
        
     }
 
-    //méthode finduser par login. login est passée par le Controller.
-    public function findUserByLogin($login) {
-        $this->db->query('SELECT * FROM utilisateurs WHERE login = :login');
-
-        //Bind 
-        $this->db->bind(':login', $login);
-        //méthode row comme objet de database
-        $row = $this->db->single();
-        return $row;
-    }
+   
 
     //Find user by email. Email is passed in by the Controller.
     public function findUserByEmail($email) {
         //Prepared statement
-        $this->db->query('SELECT * FROM utilisateurs WHERE email = :email');
+        $this->db->query('SELECT * FROM user WHERE email = :email');
 
         //Email param will be binded with the email variable
         $this->db->bind(':email', $email);
@@ -106,42 +95,6 @@ class User {
         //méthode row comme objet de database
         $user = $this->db->single();
         return $user;
-    }
+    }    
 
-    public function connected($user){
-        $this->db->query('INSERT INTO connected (id_utilisateur, str_connect) VALUES( :id_utilisateur, :str_connect)');
-        $this->db->bind('id_utilisateur', $user->id);
-        $this->db->bind('str_connect', strtotime("now") );
-        if ($this->db->execute()) {
-            return true;
-        } else {
-            return false;
-        }
-
-    }
-
-    public function cleanConnected($strt){
-        $this->db->query('DELETE FROM connected WHERE str_connect < :strt');
-        $this->db->bind('strt', $strt);
-        if ($this->db->execute()) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-   
-
-    public function disconnected($user){
-        
-        $this->db->query('DELETE FROM connected WHERE id_utilisateur = :id_utilisateur');
-        $this->db->bind('id_utilisateur', $user);
-        if ($this->db->execute()) {
-            return true;
-        } else {
-            return false;
-        }
-
-    }
-
-   
 }
