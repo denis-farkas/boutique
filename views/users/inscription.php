7e8dda1d46c52470a56e3fbb8adbc7a9a76ecb5a
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2021-02-09 14:55:05 +0100

    modif banner page inscription

---
+++ views/users/inscription.php
@@ -0,0 +1,51 @@
+<?php
+   require (ROOT.'views/includes/head.php');
+?>
+ <body>
+
+    <div class="container-fluid">
+        
+        <div class="col-md-12">
+            <?php
+            require (ROOT.'views/includes/navigation.php');
+            ?>
+        </div>
+        
+        
+        <div class="col-md-12 mt-5">
+            <div class="row banner pt-3">                               
+                <img class="ml-5 img-fluid mt-5" src="<?php echo WWW_ROOT; ?>public/images/logo.png" alt="Logo">                 
+                <h2 class="mt-5 pt-5 gold">PANAMA HATS<br /><small class="text-muted">Chapeaux de Légende</small></h2>                              
+            </div>
+        </div>                                       
+                      
+                           
+            </div>
+        </div>
+        
+       
+        <div class="row">
+            <div class="col-sm-12 col-md-3 my-5 text-center">
+                <div class="row">
+                    <aside class="col-md-12">
+                        <?php
+                        require (ROOT.'views/includes/aside.php');
+                        ?>  
+                    </aside>             
+                    
+                </div>
+            </div>
+            <section class="col-sm-12 col-md-9 my-5 text-center">
+                <div class="container">
+               
+
+
+               
+                </div>
+            </section>
+        </div>
+
+
+        <?php
+        require ROOT . '/views/includes/footer.php';
+        ?>
