eee8c81761ca5dc21e0184795965966bd9f07f47
denis farkas <denis.farkas@laplateforme.io>
2021-02-18 10:50:32 +0100

    ajout contenu nav catalogue

--- views/includes/nav_tab.php
+++ views/includes/nav_tab.php
@@ -14,15 +14,35 @@
 </ul>
 <div id="myTabContent" class="tab-content m-3">
   <div class="tab-pane fade show active" id="description">
-    <p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.</p>
-  </div>
+    <p>La fabrication traditionnelle du chapeau Panama se localise essentiellement dans trois provinces de l'Equateur: La province de Manabi et celles d' Azuay et de Cañar.
+    Chacune de ces deux régions a développé au cours de son histoire un savoir-faire particulier, des techniques de tissages différentes sur certains points.
+    Cela se traduit au niveau de la production nationale par l' identification de deux grandes familles de chapeaux: Le Montecristi et le Cuenca(Fedora).
+    Notre catalogue par souci de commodité s' articule autour de cette division.</p>
+    <p>L'ensemble de nos articles proviennent de cet artisanat équatorien: ils sont tissés à la main, avec de la palme Toquilla.</p>
+    </div>
   <div class="tab-pane fade" id="entretien">
-    <p>Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit.</p>
-  </div>
+    <p>La palme de toquilla contient un certain degré d'humidité, selon son épaisseur; ce qui lui confère sa flexibilité.
+    Quand le chapeau est exposé à une atmosphère saturée d'humidité il l'absorbe. A contrario, en atmosphère séche la palme va perdre son humidité,
+    au risque de devenir cassante.
+    Ces processus fragilisent sa structure avec le temps.
+    Les Montecristis ne connaissent pas ce problème car ils utilisent une palme plus fine et un tissage plus dense.
+    Pour protéger votre chapeau, veuillez suivre nos conseils dans la rubrique Entretien.</p>
+    </div>
   <div class="tab-pane fade" id="taille">
-    <p>Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify squid 8-bit cred pitchfork.</p>
-  </div>
+    <p>Afin de mesurer exactement  votre tour de tête nous vous conseillons d’appliquer un mètre de couturiere comme une couronne, sans exercer une trop grande pression.
+      Cette légère diagonale doit se situer un centimètre au dessus de vos oreilles.
+      Vous pouvez choisir votre taille  dans notre formulaire de commande.
+      La diversité de  production du chapeau panama et son origine artisanale nous permettent de selectionner facilement la taille adéquate.</p>
+    </div>
   <div class="tab-pane fade" id="livraison">
-    <p>Trust fund seitan letterpress, keytar raw denim keffiyeh etsy art party before they sold out master cleanse gluten-free squid scenester freegan cosby sweater. Fanny pack portland seitan DIY, art party locavore wolf cliche high life echo park Austin. Cred vinyl keffiyeh DIY salvia PBR, banh mi before they sold out farm-to-table VHS viral locavore cosby sweater.</p>
-  </div>
+    <p>Un nouveau service de livraison avec suivi apparaît pour les chapeaux  économiques.
+     L'envoi est effectué avec colissimo pour l'Europe.
+    Pour les commandes de plus de 200 euros nous n'utilisons que chronopost. 
+    - Combien de temps pour recevoir ma livraison?
+    Nous avons besoin de deux à 5 jours pour préparer votre chapeau.  
+    -Comment puis je suivre ma livraison? 
+    Nous envoyons un email avec la confirmation de l'envoi et les références du colis.</p>
+    <p>Pour les retours, veuillez vous conformer aux conditions décrites dans les conditions générales.
+     Vous disposez selon la loi, d'un délai de rétraction de 14 jours.</p>
+    </div>
 </div>
