4b63e086720c48e2fdd4751c8fc1de3ae2cdabab
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2021-02-23 10:44:04 +0100

    modif magasin

--- views/main/magasin.php
+++ views/main/magasin.php
@@ -93,6 +93,7 @@
                             <img class="img-fluid w-100 mt-2" src="<?php echo WWW_ROOT; ?>public/images/boutique.jpg" alt="boutique">
                         </div>
                     </div>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.3741769277403!2d2.3055238154314894!3d48.87014307928855!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66fc442c4314f%3A0xd746ac4b7aa7c830!2s42%20Av.%20des%20Champs-%C3%89lys%C3%A9es%2C%2075008%20Paris!5e0!3m2!1sfr!2sfr!4v1614073310120!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
                 </div>
             </section>
         </div>
