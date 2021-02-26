a235c2e88447382e31c97874cc20346a6675b65d
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2021-02-26 09:13:25 +0100

    modif icon facture

--- views/factures/afficheNofacture.php
+++ views/factures/afficheNofacture.php
@@ -79,11 +79,11 @@
                             <div class="jumbotron">
                                 <h1 class="display-5 mb-5">FACTURATION ABSENTE</h1>
                                 
-                                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-cart-x m-5" viewBox="0 0 16 16">
-  <path d="M7.354 5.646a.5.5 0 1 0-.708.708L7.793 7.5 6.646 8.646a.5.5 0 1 0 .708.708L8.5 8.207l1.146 1.147a.5.5 0 0 0 .708-.708L9.207 7.5l1.147-1.146a.5.5 0 0 0-.708-.708L8.5 6.793 7.354 5.646z"/>
-  <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-file-earmark-x" viewBox="0 0 16 16">
+  <path d="M6.854 7.146a.5.5 0 1 0-.708.708L7.293 9l-1.147 1.146a.5.5 0 0 0 .708.708L8 9.707l1.146 1.147a.5.5 0 0 0 .708-.708L8.707 9l1.147-1.146a.5.5 0 0 0-.708-.708L8 8.293 6.854 7.146z"/>
+  <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
 </svg>
-                                <p>Vous n'avez pas encore de facture enregistrée à votre nom.</p>
+                                <p class="mt-5">Vous n'avez pas encore de facture enregistrée à votre nom.</p>
                                 <p>Nous espérons que vous trouverez des articles à votre goût.</p><br>
                                 <h5><a class="nav-link" href="<?= WWW_ROOT ?>pages/index">Bonne visite, sur www.panamaHats.com</a></h5>
                             </div>
